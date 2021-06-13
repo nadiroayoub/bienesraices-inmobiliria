c212806b62054132c8d9705a149a7d98d40d1605
Nadir Ayoub <nadiroayoub@gmail.com>
2021-06-13 22:52:12 +0200

    leer las preferencias del sistema: si chrome es en dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -6,10 +6,33 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function darkMode(){
+
+    // chrome://flags/#enable-force-dark
+    const prefieresDarkMode = window.matchMedia("{prefers-color-scheme: dark}");
+    prefieresDarkMode.matches = true ? document.body.classList.add('dark-mode'):document.body.classList.remove('dark-mode'); 
+
+    // prefieresDarkMode.addEventListener('change', function(){
+    //     prefieresDarkMode.matches = true ? document.body.classList.add('dark-mode'):document.body.classList.remove('dark-mode'); 
+    // })
+
     const botonDarkMode = document.querySelector(".dark-mode-boton");
     botonDarkMode.addEventListener('click', function(){
         document.body.classList.toggle('dark-mode');
+
+        //Para que el modo elegido se quede guardado en local-storage
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('modo-oscuro','true');
+        } else {
+            localStorage.setItem('modo-oscuro','false');
+        }
     });
+
+    if (localStorage.getItem('modo-oscuro') === 'true') {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
 }
 
 function eventListeners(){
